19efe7c5a810ce18f95f9d4631e4cfef5b048cad
Flaque <econrad@zagmail.gonzaga.edu>
2015-12-04 14:22:16 -0800

    Added Address object

--- src/objects/House.java
+++ src/objects/House.java
@@ -5,13 +5,13 @@ import java.util.ArrayList;
 import mainpackage.*;
 
 public class House {
-	public String address;
+	public Address address;
 	public ArrayList<Review> reviews;   // current reviews
 	public NumericalRating overall_ratings;
 	
-	public House(String address, ArrayList<Review> reviews) {
+	public House(Address address, ArrayList<Review> reviews) {
 		this.address = address;
 		this.reviews = reviews;	
-		this.overall_ratings.ratings = reviews;
+		//this.overall_ratings.ratings = reviews;
 	}
 }
