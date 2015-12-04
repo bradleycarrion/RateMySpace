52c1a608b19de9bbf1f1137bc9cd4a3f9af045d7
Flaque <econrad@zagmail.gonzaga.edu>
2015-12-04 14:31:06 -0800

    Added address?

---
+++ src/objects/Address.java
@@ -0,0 +1,19 @@
+package objects;
+
+public class Address {
+	public String street;
+	public String city;
+	public String state;
+	public int zip;
+	
+	public Address(String street, String city, String state, int zip) {
+		this.street = street;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+	}
+	
+	public String asString() {
+		return street + ", " + city + ", " + state + ", " + Integer.toString(zip);
+	}
+}
